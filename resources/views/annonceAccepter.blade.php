d9f04884fde8938e01ddbcf4410bd584bc3787aa
Adama Gueye <adamarahma99@gmail.com>
2024-02-19 13:04:02 +0000

    mdf mailling

--- resources/views/annonceAccepter.blade.php
+++ resources/views/annonceAccepter.blade.php
@@ -6,18 +6,38 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <title>Document</title>
 </head>
+
+<style>
+  body {
+    margin: 0;
+    padding: 0;
+  }
+  .bg-dark {
+    background-color: black;
+    padding: 20px;
+  }
+  h1 {
+    color: white;
+    font-family: Arial, sans-serif;
+    font-size: 48px;
+    text-align: center;
+  }
+  h5 {
+    color: midnightblue; /* Couleur bleu de nuit */
+    font-family: Arial, sans-serif;
+    font-size: 36px;
+    font-style: italic; /* Texte en italique */
+  }
+</style>
 <body>
 
     <div class="container">
-        <div class="bg-dark mt-5">
-            <img src="images/logo2-removebg-preview.png" height="100" width="300">
-        </div>
-        <div class="mt-2" style="text-align: center;">
-            <img src="images/202402010745img3.jpg" height="300" width="1000">
+        <div class="bg-dark mt-2">
+            <h1 style="color: white;">AUTOCONNECT</h1>
         </div>
 
-        <div class="mt-5 mb-1">
-            <img src="images/logo2-removebg-preview-removebg-preview.png" height="50" width="100">
+        <div class="mt-2" style="text-align: center;">
+            <img src="https://media.istockphoto.com/id/1221473473/nl/foto/koper-van-auto-die-handen-met-verkoper-in-autohandel-schudt-mening-van-binnenland-van-auto.jpg?s=612x612&w=0&k=20&c=GGnhGLvqmKZUjVNwaBs578X_DKBl8E3s2XfmFNBeWUo=" height="300" width="1000">
         </div>
 
         <div>
@@ -29,7 +49,7 @@
                     maintenant Veuillez cliquer sur le lien <br>
                     ci-dessous pour voir votre annonce sur la plateforme 😍.
                 </p>
-                <img src="images/202401312158img2.jpg" alt="" height="100" width="300">
+                <img src="https://media.istockphoto.com/id/1222996765/nl/foto/sluit-omhoog-van-handelaar-die-sleutel-geeft-aan-nieuwe-eigenaar-en-handen-schudt-in-autotoont.jpg?s=612x612&w=0&k=20&c=5Zt3AJLvnrUa0HwlT7Xv_XlioQywt06DNUNIaaplyCk=" alt="" height="100" width="300">
             </div>
             <a href="http://localhost:4200/vehicules" class="btn btn-primary w-25" >Voir annonce</a>
 
--- resources/views/newsletter.blade.php
+++ resources/views/newsletter.blade.php
@@ -4,22 +4,41 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <title>Document</title>
+    <title>AUTOCONNECT</title>
 </head>
+
+<style>
+  body {
+    margin: 0;
+    padding: 0;
+  }
+  .bg-dark {
+    background-color: black;
+    padding: 20px;
+  }
+  h1 {
+    color: white;
+    font-family: Arial, sans-serif;
+    font-size: 48px;
+    text-align: center;
+  }
+  h5 {
+    color: midnightblue; /* Couleur bleu de nuit */
+    font-family: Arial, sans-serif;
+    font-size: 36px;
+    font-style: italic; /* Texte en italique */
+  }
+</style>
+
 <body>
 
     <div class="container">
-        <div class="bg-dark mt-5">
-            <img src="images/logo2-removebg-preview.png" height="100" width="300">
+        <div class="bg-dark mt-2">
+            <h1 style="color: white;">AUTOCONNECT</h1>
         </div>
         <div class="mt-2" style="text-align: center;">
-            <img src="images/202402010745img3.jpg" height="300" width="1000">
+            <img src="https://media.istockphoto.com/id/1221473473/nl/foto/koper-van-auto-die-handen-met-verkoper-in-autohandel-schudt-mening-van-binnenland-van-auto.jpg?s=612x612&w=0&k=20&c=GGnhGLvqmKZUjVNwaBs578X_DKBl8E3s2XfmFNBeWUo=" height="300" width="1000">
         </div>
-
-        <div class="mt-5 mb-1">
-            <img src="images/logo2-removebg-preview-removebg-preview.png" height="50" width="100">
-        </div>
-
         <div>
             <h5>Chére/Cher Client(e)</h5>
             <div class="d-flex justify-content-between align-content-center">
@@ -29,12 +48,9 @@
                     Veuillez vite jeter un coup d'oeil ca pourrait <br>
                     correspondre à votre besoin 😍.
                 </p>
-                <img src="images/202401312158img2.jpg" alt="" height="100" width="300">
+                <img src="https://media.istockphoto.com/id/1222996765/nl/foto/sluit-omhoog-van-handelaar-die-sleutel-geeft-aan-nieuwe-eigenaar-en-handen-schudt-in-autotoont.jpg?s=612x612&w=0&k=20&c=5Zt3AJLvnrUa0HwlT7Xv_XlioQywt06DNUNIaaplyCk=" alt="" height="100" width="300">
             </div>
             <a href="http://localhost:4200/vehicules" class="btn btn-primary w-25" >Voir les résultats</a>
-
-            
-
         </div>
         
     </div>
